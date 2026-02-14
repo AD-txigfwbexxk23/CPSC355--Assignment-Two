8d57485ac5c6f1d7aedbddde06770a0c78eeffe6
zehaan.walji <zehaan.walji@ucalgary.ca>
2026-02-14 14:08:42 -0700

    Test to ensure that I can run

--- test.s
+++ test.s
@@ -1,23 +1,17 @@
         .section .rodata
-fmt:    .string "Hello from CPSC 355! x = %d\n"
+fmt:    .string "Meaning of life = %d\n"
 
         .text
         .global main
-
 main:
-        // Prologue
         stp     x29, x30, [sp, -16]!
         mov     x29, sp
 
-        // printf(fmt, 42)
         adrp    x0, fmt
         add     x0, x0, :lo12:fmt
         mov     w1, #42
         bl      printf
 
-        // return 0
         mov     w0, #0
-
-        // Epilogue
         ldp     x29, x30, [sp], 16
         ret
