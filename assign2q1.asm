e1431e99969a6cfa44a5d968a344dace04ae7e88
zehaan.walji <zehaan.walji@ucalgary.ca>
2026-02-14 18:58:57 -0700

    Assignment Question one

--- assign2q1.asm
+++ assign2q1.asm
@@ -1,39 +1,53 @@
-output: .string "The output of these values is: %d\n"
-.balign 2
+//Zehaan Walji
+//Sat Feb 3 2024
+//CPSC355- Assignment Two - Question One
+
+
+
+//Creating labels for the input and output format strings
+.data
+input0: .string "%ld"
+output0: .string "%ld\n"
+
+//Reserving space for the variable 'num' in the .bss section
+.bss //--> Where you put uninitialized data
+.balign 8
+num: .skip 8 //Reserve 8 bytes for the variable 'num'
+
+
+
+.text
 .global main //Makes the function visible to the linker
+.extern scanf
+.extern printf
 
 
 // ----------------------------------------------------------------------//
+//MAIN FUNCTION
 
 main: stp x29, x30, [sp, #-16]! //Push the frame pointer and return address onto the stack
 mov x29, sp //Set the frame pointer to the current stack pointer
 
-//Calulcating answer
-mov x19, #2
-mov x21, #3
-mov x22, #1
-mneg x20, x21, x22 //-3
-mov x21, #5
-mov x22, #7
-mov x23, #4
+
+//CODE BODY
+ldr x0, =input0 //Load the address of the input format string into x0
+ldr x1, =num //Load the address of the variable 'num' into x1
+bl scanf //Call the scanf function to read an integer from the user
+
+//Calculate the answer:
+ldr x14, =num //Load the value of 'num' into x14
+ldr x14, [x14] //Get the value at the actual memory location
+
+add x15, x14, #1
+add x16, x14, #2 
+mul x17, x14, x15 //Calculate num * (num + 1)
+mul x17, x17, x16 //Calculate num * (num + 1) * (num + 2)
+
+ldr x0, =output0 //Load the address of the output format string into x0
+mov x1, x17 //Move the result of the calculation into x1
+bl printf //Call the printf function to print the result
 
 
-mul x0, x23, x23, x23 //x^3
-mul x1, x23, x23 //x^2
-
-mul x2, x0, x19 //ax^3
-mul x3, x1, x20 //bx^2
-mul x4, x23, x21 //cx^1
-
-add x5, x2, x3 //ax^3 + bx^2
-add x6, x5, x4 //ax^3 + bx^2 + cx
-add x7, x6, x22 //ax^3 + bx^2 + cx + d
-
-
-ldr x0, =output
-move x1, x7
-bl printf //Call printf to print the output
-
-
+//RETURN
 ldp x29, x30, [sp], #16 //Pop the frame pointer and return address from the stack
 ret
