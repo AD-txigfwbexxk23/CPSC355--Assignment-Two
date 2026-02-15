ba5bc8eed985fc12d1b19f483ae460e7becb31b2
zehaan.walji <zehaan.walji@ucalgary.ca>
2026-02-14 18:11:35 -0700

    Excercise

--- assign2q1.asm
+++ assign2q1.asm
@@ -1,27 +1,35 @@
-fmt: .string "Meaning of life\n %d\n" //Define a string with a format specifier for an integer
-
-.balign 4 //Align the string to an 8-byte boundary
+output: "The output of these values is: %d\n"
 .global main //Makes the function visible to the linker
 
+
+// ----------------------------------------------------------------------//
 
 main: stp x29, x30, [sp, #-16]! //Push the frame pointer and return address onto the stack
 mov x29, sp //Set the frame pointer to the current stack pointer
 
+//Calulcating answer
+mov x19, #2
+mneg x20, #1,#3
+mov x21, #5
+mov x22, #7
+mov x23, #4
 
 
+mul x0, x23, x23, x23 //x^3
+mul x1, x23, x23 //x^2
+
+mul x2, x0, x19 //ax^3
+mul x3, x1, x20 //bx^2
+mul x4, x23, x21 //cx^1
+
+add x5, x2, x3 //ax^3 + bx^2
+add x6, x5, x4 //ax^3 + bx^2 + cx
+add x7, x6, x22 //ax^3 + bx^2 + cx + d
 
 
-
-ldr x0,=fmt
-mov w1, 42
-bl printf //Call printf to print the string
-
-
-
-
-
-
-
+ldr x0, =output
+move x1, x7
+bl printf //Call printf to print the output
 
 
 ldp x29, x30, [sp], #16 //Pop the frame pointer and return address from the stack
